5acf93e9042f6d2e1d087b6b62be417fadcd3878
Guilherme R. <guilhermerebrito@gmail.com>
2025-01-30 15:59:44 -0300

    Atualizaçoes

--- funcao_sortear.py
+++ funcao_sortear.py
@@ -1,9 +1,12 @@
 from random import randint
 from time import sleep
 
-def sorteio(lista, valores):
-    print(f'Sorteando {valores} valor(es) da lista: ', end= '')
-    for c in range(0, valores):
+def sorteio(lista, valor):
+    if valor == 1:
+        print(f'Sorteando {valor} valor da lista: ', end= '')
+    else:
+        print(f'Sorteando {valor} valor(es) da lista: ', end= '')
+    for c in range(0, valor):
         n = randint(0, 10)
         numero.append(n)
         print(n, end=' ', flush=True)
@@ -12,16 +15,40 @@ def sorteio(lista, valores):
 
 def sortepar(lista):
     soma = 0
+    pares = []
     for item in lista:
         if item % 2 == 0:
             soma += item
-    print(f'Somando os valores pares: {lista}, temos {soma}.')
+            pares.append(item)
+    print(f'Somando os valores pares temos: {soma}. Que são: ', end='')
+    for j in pares:
+        print(j, end=' ')
+    print('')
+
+
+def sorteimpar(lista):
+    soma = 0
+    impares = []
+    for item in lista:
+        if item % 2 == 1:
+            soma += item
+            impares.append(item)
+    print(f'Somando os valores impares temos: {soma}. Que são: ', end='')
+    for j in impares:
+        print(j, end=' ')
+    print('')
+
+
 
 while True:
     valores = input('Quantos numeros você quer sortear? ')
+
     if valores.isnumeric():
         valores = int(valores)
-        break
+        if valores == 0:
+            print('Por favor, digite um valor maior que 0!')
+        else:
+            break
     else:
         print('Por favor, digite um valor valido!')
 
@@ -29,3 +56,4 @@ while True:
 numero = []
 sorteio(numero, valores)
 sortepar(numero) 
+sorteimpar(numero)
